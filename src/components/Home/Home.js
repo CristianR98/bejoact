0855732235763ecce67001de36a68ecc0ad96ce3
Cristian <cristian.roman@mdevz.com>
2020-09-13 20:39:15 -0300

    - added catalog.json for used as test data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import ProductCard from '../ProductCard/ProductCard';
 import './Home.css';
+import catalog from '../../catalog.json';
 
-const Home = (props) => {
-    return (   
-        <div className="home-container">
-            <header className="main-header">
-                <h1>{props.greeting}</h1>
-                <p>{props.subtitle}</p>
-            </header>
-            <div class="product-container">
-                <ProductCard/>
+class Home extends React.Component {
+
+    state = {
+        products: []
+    };
+
+    componentDidMount() {
+        let productRequest = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(catalog);
+            }, 2000);
+        });
+        productRequest
+        .then(catalog => {
+            this.setState(state => ({products: catalog}));
+        });
+    }
+
+    render () {
+        return (   
+            <div className="home-container">
+                <header className="main-header">
+                    <h1>{this.props.greeting}</h1>
+                    <p>{this.props.subtitle}</p>
+                </header>
+                <div className="product-container row">
+                    {this.state.products.map(product => 
+                        <div className="col-md-4">
+                            <ProductCard product={product}/>
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Home;
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -6,11 +6,10 @@ class ProductCard extends React.Component {
 
     state = {
         itemCount: 0,
-        stock: 5
     }
 
     addProduct() {
-        if (this.state.itemCount < this.state.stock) {
+        if (this.state.itemCount < this.props.product.stock) {
             this.setState(state => ({itemCount: state.itemCount + 1}));
         }
     }
@@ -26,15 +25,16 @@ class ProductCard extends React.Component {
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="https://picsum.photos/400/200" />
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>{this.props.product.name}</Card.Title>
                     <Card.Text>
                     Some quick example text to build on the card title and make up the bulk of
                     the card's content.
                     </Card.Text>
-                    <p class="stock">Stock: {this.state.stock}</p>
-                    <div class="product-actions">
+                    <p className="price">$ {this.props.product.price}</p>
+                    <p className="stock">Stock: {this.props.product.stock}</p>
+                    <div className="product-actions">
                         <Button variant="primary" onClick={this.addProduct.bind(this)} >+</Button>
-                        <span class="item-count">{this.state.itemCount}</span>
+                        <span className="item-count">{this.state.itemCount}</span>
                         <Button variant="primary" onClick={this.substractProduct.bind(this)}>-</Button>
                     </div>
                 </Card.Body>
